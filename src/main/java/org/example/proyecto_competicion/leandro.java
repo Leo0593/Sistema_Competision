a81db1cf30858a91a51c0608c4490542ef00e4f5
Leo0593 <leandroclavijo854@gmail.com>
2025-01-09 09:18:55 +0100

    hola mundo bebe

--- src/main/java/org/example/proyecto_competicion/leandro.java
+++ src/main/java/org/example/proyecto_competicion/leandro.java
@@ -2,6 +2,6 @@ package org.example.proyecto_competicion;
 
 public class leandro {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); //hola
+        System.out.println("Hello, World!"); //hola hjkas
     }
 }
