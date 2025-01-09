184549c27020c92fbdbbad34fc3ad1e207fe50c7
Leo0593 <leandroclavijo854@gmail.com>
2025-01-09 08:52:48 +0100

    Initial commit

---
+++ src/main/java/org/example/proyecto_competicion/ProyectoCompeticionApplication.java
@@ -0,0 +1,13 @@
+package org.example.proyecto_competicion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoCompeticionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoCompeticionApplication.class, args);
+    }
+
+}
