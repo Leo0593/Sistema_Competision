644f76a1913c9c223062a7faee16a4895a89037a
Leo0593 <leandroclavijo854@gmail.com>
2025-01-09 12:50:32 +0100

    dad

--- src/main/java/org/example/proyecto_competicion/Escibe_un_comentario_para_subir.java
+++ src/main/java/org/example/proyecto_competicion/Escibe_un_comentario_para_subir.java
@@ -5,5 +5,5 @@ public class Escibe_un_comentario_para_subir {
     // Estoy creando el login de la aplicación
     //borre el das
     //Agregue los repositorios
-    //dasdasd
+    //dasdasddasdasd
 }
--- src/main/java/org/example/proyecto_competicion/Repository/CompeticionRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.proyecto_competicion.Repository;
-
-import Models.Competicion;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompeticionRepository extends JpaRepository<Competicion, Integer> {
-}
