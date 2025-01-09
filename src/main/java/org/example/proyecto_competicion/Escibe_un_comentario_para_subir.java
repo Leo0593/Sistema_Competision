d37de4d690c1b21725921f053e68d8c4a1702ac9
steve <stevejph48@gmail.com>
2025-01-09 12:55:52 +0100

    aa

--- src/main/java/org/example/proyecto_competicion/Escibe_un_comentario_para_subir.java
+++
@@ -1,9 +0,0 @@
-package org.example.proyecto_competicion;
-
-public class Escibe_un_comentario_para_subir {
-
-    // Estoy creando el login de la aplicación
-    //borre el das
-    //Agregue los repositorios
-    //dasdasd
-}
