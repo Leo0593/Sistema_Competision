1df7c7931c17ab576529967ea2f13ab45b9865de
17Mireia18 <miralipalco@gmail.com>
2025-01-10 09:15:14 +0100

    hh

--- src/main/java/org/example/proyecto_competicion/Escribe_Comentarios.java
+++ src/main/java/org/example/proyecto_competicion/Escribe_Comentarios.java
@@ -3,5 +3,6 @@ package org.example.proyecto_competicion;
 public class Escribe_Comentarios {
     public static void main(String[] args) {
         System.out.println("Escribe un comentario para subidasdasddasdasdar");
+        System.out.println("Comentario añadido por el usuario 2");
     }
 }
