72ef30951e21ef7acea30a3d5590ce5a4ab3835c
Leo0593 <leandroclavijo854@gmail.com>
2025-01-14 11:48:20 +0100

    registrosfsdf

--- src/main/java/org/example/proyecto_competicion/Escribe_Comentarios.java
+++ src/main/java/org/example/proyecto_competicion/Escribe_Comentarios.java
@@ -4,7 +4,7 @@ public class Escribe_Comentarios {
     public static void main(String[] args) {
         System.out.println("Escribe un comentario para subidasdasddasdasdar");
         System.out.println("Comentario añadido por el usuario 2");
-        System.out.println("Comentario añadido por el usuario 2");
+        System.out.println("Comentario añadido por el usadasdsaduario 2");
 
     }
 }
